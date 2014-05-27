619d23fabb3c87862914ba1fb66a31219c76a458
Thibault92 <thibault.berger92@gmail.com>
2014-05-28 00:59:00 +0200

    Create redDetect.m
    Détection des contours rouges

---
+++ redDetect.m
@@ -0,0 +1,39 @@
+function imdetect = redDetect(img,Cb,Cr)
+
+[H,W,Canaux] = size(img);
+
+% for i = 1:H
+%     for j = 1:W
+% %         if img(i,j,1) < 0.5 && img(i,j,2) < 0.1 && img(i,j,3) < 0.1
+%         if  img(i,j,1)/img(i,j,2) > 1.25 && img(i,j,1)/img(i,j,3) > 1.4 ...
+%                 || img(i,j,3)/img(i,j,1) > 1.2 && img(i,j,3)/img(i,j,2) > 1.2
+%             imdetect(i,j,:) = 1;
+%         else imdetect(i,j,:) = 0;
+%         end
+%     end
+% end
+
+% Détection des contours en YCbCr (prend en compte la luminance et deux caractéristiques de chrominance)
+
+% Cb = imgYCbCr(:,1,:);
+% Cr = imgYCbCr;
+
+
+for i = 1:H
+    for j = 1:W
+        if  Cb(i,j,2) > 0.4 && Cb(i,j,2) < 0.6 ...
+            && Cr(i,j,3) > 0.5 && Cr(i,j,3) < 0.8
+            
+%         if  imgYCbCr(i,j,2)/imgYCbCr(i,j,3) > 0.6 && imgYCbCr(i,j,2)/imgYCbCr(i,j,3) < 0.9 ...
+%             && imgYCbCr(i,j,3)/imgYCbCr(i,j,2) > 1.1 && imgYCbCr(i,j,3)/imgYCbCr(i,j,2) < 1.5
+
+%         if  imgYCbCr(i,j,3)/imgYCbCr(i,j,1) > 1.6 && imgYCbCr(i,j,3)/imgYCbCr(i,j,1) < 2.2 ...
+%             || imgYCbCr(i,j,2)/imgYCbCr(i,j,1) > 1.8 && imgYCbCr(i,j,2)/imgYCbCr(i,j,1) < 3
+
+            imdetect(i,j,:) = 1;
+            
+        else imdetect(i,j,:) = 0;
+        end
+    end
+end
+end
